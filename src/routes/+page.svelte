86815a44529263d00260f77b2b232c6d2c869d73
jwklong <jwklong@gmx.com>
2026-09-24 17:11:11 +0100

    placeholder
    Signed-off-by: jwklong <jwklong@gmx.com>

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<div>
+    👀 coming soon
+</div>
+
+<style>
+    div {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+        font-size: 2rem;
+        font-weight: bold;
+        font-family: sans-serif;
+    }
+</style>
