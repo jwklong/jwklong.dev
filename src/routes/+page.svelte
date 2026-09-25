e90fe66d45634badfc44fe875dc75b8c3ba2b718
jwklong <jwklong@gmx.com>
2026-09-25 08:03:09 +0100

    social icons
    Signed-off-by: jwklong <jwklong@gmx.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
     import Icon from "$lib/components/icon/icon.svelte";
 
+    import youtubeIcon from "$lib/assets/youtube.png";
+    import githubIcon from "$lib/assets/github.png";
+    import penguinmodIcon from "$lib/assets/penguinmod.png";
 
 </script>
 
@@ -8,6 +11,17 @@
     <Icon size="256px" />
     <div class="info">
         <h1>jwklong</h1>
+        <div class="socials">
+            <a href="https://youtube.com/@jwklong" target="_blank">
+                <img src={youtubeIcon} alt="youtube" />
+            </a>
+            <a href="https://github.com/jwklong" target="_blank">
+                <img src={githubIcon} alt="github" />
+            </a>
+            <a href="https://penguinmod.com/profile?user=jwklong" target="_blank">
+                <img src={penguinmodIcon} alt="penguinmod" />
+            </a>
+        </div>
     </div>
 </div>
 
@@ -28,9 +42,20 @@
         height: 256px;
         align-items: flex-start;
         justify-content: flex-start;
+        flex-direction: column;
     }
 
     .info h1 {
         margin: 0;
+    }
+
+    .socials {
+        height: 32px;
+    }
+
+    .socials img {
+        height: 32px;
+        aspect-ratio: 1;
+        object-fit: contain;
     }
 </style>
